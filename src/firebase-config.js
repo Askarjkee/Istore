52c57801b38264f746aac76cac6ba14390234af6
Askar Askarjkee Arman <askarjkee2jzz@gmail.com>
2022-01-25 17:58:41 +0600

    env

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -6,12 +6,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBq8YLQG7BiyFVLgsmyZS00-BtvlNKU8-U",
-    authDomain: "istore-48532.firebaseapp.com",
-    projectId: "istore-48532",
-    storageBucket: "istore-48532.appspot.com",
-    messagingSenderId: "203766624504",
-    appId: "1:203766624504:web:466f20ab81ceeee5c38b0d"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
